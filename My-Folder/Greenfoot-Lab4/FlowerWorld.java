4e16c63e060939e9d6f66b390295938b1a56e675
lmurill3 <lmurill3@stumail.jccc.edu>
2016-10-27 13:27:19 -0500

    Add files via upload

---
+++ My-Folder/Greenfoot-Lab4/FlowerWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class FlowerWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FlowerWorld extends World
+{
+
+    /**
+     * Constructor for objects of class FlowerWorld.
+     * 
+     */
+    public FlowerWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
